65c84a59c24e2d6cc552059419ed3513c719c96c
Thomas Dettbarn <det@iis.fhg.de>
2024-11-17 13:01:33 +0100

    for some reason, it is not updating the window.

--- experiments/gtktest/picturetest.c
+++ experiments/gtktest/picturetest.c
@@ -27,6 +27,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 #include "dMagnetic2_errorcodes.h"
 #include "dMagnetic2_graphics.h"        // for the xpm function
 #include <gtk/gtk.h>
@@ -40,6 +41,10 @@ typedef struct _tHandle
 	GtkWidget *picture;
 	GdkPixbuf *pixbuf;
 	GdkTexture *texture;
+	int ping0_pong1;
+
+	GtkWidget *button;
+	GtkWidget *box;
 	unsigned char drawbuf[4*640*480];
 
 	tdMagnetic2_canvas_small canvas_small;
@@ -48,7 +53,52 @@ typedef struct _tHandle
 	void *pTmpBuf;
 	int picnum;
 	unsigned char pGfxBuf[1<<22];
+
+     	pthread_mutex_t mutex;
 } tHandle;
+
+static gboolean heartbeat(gpointer user_data)
+{
+        tHandle* pThis=(tHandle*)user_data;
+	pthread_mutex_lock(&(pThis->mutex));
+	gtk_widget_queue_draw(pThis->window);
+	pthread_mutex_unlock(&(pThis->mutex));
+	return G_SOURCE_CONTINUE;
+	
+}
+
+static void next_clicked(GtkWidget *widget,gpointer user_data)
+{
+        tHandle* pThis=(tHandle*)user_data;
+
+	int retval;
+
+	pthread_mutex_lock(&(pThis->mutex));
+	retval=dMagnetic2_graphics_decode_by_picnum(pThis->pGfxHandle,pThis->picnum,&(pThis->canvas_small),NULL);
+	printf("decoded %dx%d pixels\n",pThis->canvas_small.width,pThis->canvas_small.height);
+	if (retval==DMAGNETIC2_OK && pThis->canvas_small.width>0 && pThis->canvas_small.height>0)
+	{
+		int width;
+		int height;
+		int retval;
+		GdkPixbuf *pixbuf;
+		retval=dMagnetic2_graphics_canvas_small_to_8bit(&(pThis->canvas_small),1,pThis->drawbuf,&width,&height);
+		printf("retval:%d width:%d height:%d\n",retval,width,height);
+
+
+		pixbuf=gdk_pixbuf_new_from_data(pThis->drawbuf,
+			GDK_COLORSPACE_RGB,TRUE,8,
+			width,height,
+			width*4,NULL,NULL);
+		
+		gdk_pixbuf_copy_area(pixbuf,0,0,gdk_pixbuf_get_width(pixbuf),gdk_pixbuf_get_height(pixbuf),pThis->pixbuf,0,0);
+		g_object_unref(pixbuf);
+
+
+	}
+	pThis->picnum=(pThis->picnum+1)%32;
+	pthread_mutex_unlock(&(pThis->mutex));
+}
 
 static void activate(GtkApplication* app,gpointer user_data)
 {
@@ -59,19 +109,40 @@ static void activate(GtkApplication* app,gpointer user_data)
         gtk_window_set_title(GTK_WINDOW(pThis->window),"hello there!");
         gtk_window_set_default_size(GTK_WINDOW(pThis->window),640,200);
 
-        pThis->picture=gtk_picture_new();
-        pThis->pixbuf=gdk_pixbuf_new(GDK_COLORSPACE_RGB,FALSE,8,640,200);
+
+	pThis->box=gtk_box_new(GTK_ORIENTATION_VERTICAL,0);
+	gtk_widget_set_halign (pThis->box, GTK_ALIGN_CENTER);
+	gtk_widget_set_valign (pThis->box, GTK_ALIGN_CENTER);
+
+	gtk_window_set_child(GTK_WINDOW(pThis->window),pThis->box);
+	pThis->button=gtk_button_new_with_label("next");
+	g_signal_connect(pThis->button,"clicked",G_CALLBACK(next_clicked),pThis);
+	gtk_box_append(GTK_BOX(pThis->box),pThis->button);
+
+
+
+        pThis->picture=gtk_picture_new_for_paintable(NULL);
+	gtk_widget_set_visible(pThis->picture,true);
+
+ //       pThis->pixbuf=gdk_pixbuf_new(GDK_COLORSPACE_RGB,FALSE,8,640,200);
+	{
+		int i;
+		for (i=0;i<640*200*4;i+=3) pThis->drawbuf[i]=0xff;
+	}
+	pThis->pixbuf=gdk_pixbuf_new_from_data(pThis->drawbuf, GDK_COLORSPACE_RGB,TRUE,8,640,200,640*4,NULL,NULL);
         pThis->texture=gdk_texture_new_for_pixbuf(pThis->pixbuf);
         gtk_picture_set_paintable(GTK_PICTURE(pThis->picture),GDK_PAINTABLE(pThis->texture));
 
+	gtk_box_append(GTK_BOX(pThis->box),pThis->picture);
 
-        gtk_window_set_child(GTK_WINDOW(pThis->window),pThis->picture);
         g_signal_connect(G_OBJECT(pThis->window),"close_request",G_CALLBACK(gtk_window_destroy),pThis->window);
         gtk_window_present(GTK_WINDOW(pThis->window));
 
+        pthread_mutex_init(&(pThis->mutex),NULL);
+    	g_timeout_add(19,heartbeat,pThis);  // every 19 ms call the heartbeat
 }
 
-
+tHandle handle;
 int main(int argc,char** argv)
 {
 	GtkApplication *app;
@@ -82,21 +153,29 @@ int main(int argc,char** argv)
 	int size_tmpbuf;
 	FILE *f;
 	int gfxsize;
-	pHandle=malloc(sizeof(tHandle));
-	memset(pHandle,0,sizeof(tHandle));
+	memset(&handle,0,sizeof(tHandle));
 
 	retval=dMagnetic2_graphics_getsize(&size_handle,&size_tmpbuf);
-	pHandle->pGfxHandle=malloc(size_handle);
-	pHandle->pTmpBuf=malloc(size_tmpbuf);
+	handle.pGfxHandle=malloc(size_handle);
+	handle.pTmpBuf=malloc(size_tmpbuf);
 
-	retval=dMagnetic2_graphics_init(pHandle->pGfxHandle,pHandle->pTmpBuf);
+	retval=dMagnetic2_graphics_init(handle.pGfxHandle,handle.pTmpBuf);
 	
 	f=fopen("guild.gfx","rb");
-	gfxsize=fread(pThis->pGfxBuf,sizeof(char),sizeof(pThis->pGfxBuf),f);
+	gfxsize=fread(handle.pGfxBuf,sizeof(char),sizeof(handle.pGfxBuf),f);
 	fclose(f);
 
-	dMagnetic2_graphics_set_gfx(pHandle->pGfxHandle,pThis->pGfxBuf,gfxsize);
-	pThis->picnum=0;
+	dMagnetic2_graphics_set_gfx(handle.pGfxHandle,handle.pGfxBuf,gfxsize);
+
+	handle.picnum=0;
+
+
+
+	app=gtk_application_new(NULL,G_APPLICATION_DEFAULT_FLAGS);
+	g_signal_connect(app,"activate",G_CALLBACK(activate),&handle);
+	status=g_application_run(G_APPLICATION(app),argc,argv);
+	g_object_unref(app);
+	return status;
 }
 
 
