80b4811a26f12779589bcdbfade67dea184bc1c7
Thomas Dettbarn <dettus@dettus.net>
2025-04-18 22:17:41 +0200

    the picture is being scale with the window now.

--- experiments/gtktest/picturetest2.c
+++ experiments/gtktest/picturetest2.c
@@ -40,11 +40,14 @@ typedef struct _tGUIHandle
 {
 	GtkApplication *app;
 	GtkWidget *window;
+	GtkWidget *box;
+	GtkWidget *button;
 	GtkWidget *picture;
-	GdkPixbuf *pixbuf;
+	GdkPixbuf *pixbuf_ping;
+	GdkPixbuf *pixbuf_pong;
 	unsigned char drawbuf[4*MAX_WIDTH*MAX_HEIGHT];	// red, green, blue, alpha
 	GdkTexture *texture_ping;
-	GdkTexture *texture_poing;
+	GdkTexture *texture_pong;
 	int ping0_pong1;
 	pthread_mutex_t mutex;
 	tdMagnetic2_canvas_small canvas_small;
@@ -116,7 +119,73 @@ static void gui_activate(GtkApplication* app,gpointer user_data)
 	pGUI->window=gtk_application_window_new(pGUI->app);
 	gtk_window_set_title(GTK_WINDOW(pGUI->window),"hello there!");	
 	g_signal_connect(G_OBJECT(pGUI->window),"close_request",G_CALLBACK(gtk_window_destroy),pGUI->window);
+
+
+	pGUI->box=gtk_box_new(GTK_ORIENTATION_VERTICAL,0);
+	gtk_widget_set_halign(pGUI->box,GTK_ALIGN_BASELINE_FILL);
+	gtk_widget_set_valign(pGUI->box,GTK_ALIGN_START);
+	gtk_widget_set_hexpand(pGUI->box,true);
+	gtk_widget_set_vexpand(pGUI->box,true);
+//	gtk_window_set_child(GTK_WINDOW(pGUI->window),pGUI->box);
+
+
+	pGUI->button=gtk_button_new_with_label("next");
+	gtk_widget_set_hexpand(pGUI->button,true);
+	gtk_widget_set_vexpand(pGUI->button,false);
+	//g_signal_connect(pGUI->button,"clicked",G_CALLBACK(gui_next_clicked),pThis);
+	gtk_box_append(GTK_BOX(pGUI->box),pGUI->button);
+
+
+	pGUI->picture=gtk_picture_new_for_paintable(NULL);
+	gtk_widget_set_visible(pGUI->picture,true);
+	gtk_widget_set_hexpand(pGUI->picture,true);
+	gtk_widget_set_vexpand(pGUI->picture,true);
+
+	pGUI->pixbuf_ping=gdk_pixbuf_new(GDK_COLORSPACE_RGB,true,8,640,480);
+	pGUI->pixbuf_pong=NULL;
+	{
+		GdkPixbuf *pixbuf;
+		int i;
+		for (i=0;i<640*480;i++)
+		{
+			pGUI->drawbuf[4*i+0]=0x55;
+			pGUI->drawbuf[4*i+1]=0x55;
+			pGUI->drawbuf[4*i+2]=0xff;
+			pGUI->drawbuf[4*i+3]=0xff;
+		}
+		for (i=0;i<480;i++)
+		{
+			pGUI->drawbuf[4*(640*i+i)+0]=0xff;
+			pGUI->drawbuf[4*(640*i+i)+1]=0x55;
+			pGUI->drawbuf[4*(640*i+i)+2]=0x55;
+			pGUI->drawbuf[4*(640*i+i)+3]=0xff;
+		}
+		pixbuf=gdk_pixbuf_new_from_data(pGUI->drawbuf,GDK_COLORSPACE_RGB,TRUE,8,640,480,640*4,NULL,NULL);
+		gdk_pixbuf_copy_area(pixbuf,0,0,gdk_pixbuf_get_width(pixbuf),gdk_pixbuf_get_height(pixbuf),pGUI->pixbuf_ping,0,0);	
+		g_object_unref(pixbuf);
+	}
+	pGUI->texture_ping=gdk_texture_new_for_pixbuf(pGUI->pixbuf_ping);
+	pGUI->texture_pong=NULL;
+	pGUI->ping0_pong1=0;
+
+	gtk_picture_set_paintable(GTK_PICTURE(pGUI->picture),GDK_PAINTABLE(pGUI->texture_ping));
+	gtk_box_append(GTK_BOX(pGUI->box),pGUI->picture);
+
+		
+	
+	
+
+
+
+	//gtk_window_set_child(GTK_WINDOW(pGUI->window),pGUI->picture);
+	gtk_window_set_child(GTK_WINDOW(pGUI->window),pGUI->box);
+
+
 	gtk_window_present(GTK_WINDOW(pGUI->window));
+
+
+
+	
 
 }
 
